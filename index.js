fd937e1d86774ba799575a17fb633bf323185420
Googler <knowbeeinc@gmail.com>
2020-03-07 15:41:32 +0200

    skip open networks

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
-const { readFileSync, readdirSync, existsSync } = require("fs");
+const {
+  readFileSync,
+  readdir,
+  unlink,
+  readdirSync,
+  existsSync
+} = require("fs");
 const path = require("path");
 const { exec } = require("child_process");
 
@@ -6,36 +12,57 @@ const credentials = path.join() + "/credentials";
 let passwords = [];
 
 keepCredentials = profiles => {
+  // deleting all files from credentials folder first
+  readdirSync(credentials, (err, files) => {
+    if (err) throw err;
+
+    for (const file of files) {
+      unlink(path.join(credentials, file), err => {
+        if (err) throw err;
+      });
+    }
+  });
   for (const profile of profiles) {
     exec(
-      `netsh wlan show profiles ${profile} key=clear > ${credentials}/${profile}`
+      `netsh wlan show profiles "${profile}" key=clear > ${credentials}/"${profile}"`
     );
   }
-  // const exist = existsSync(credentials);
-  // if (exist) {
-  //   const dirnames = readdirSync(credentials);
-  //   for (const filename of dirnames) {
-  //     const data = readFileSync(`${credentials}/${filename}`);
-  //     try {
-  //       const name = data
-  //         .toString()
-  //         .split(" on interface WiFi")[0]
-  //         .split("Profile ")[1];
-  //       const password = data
-  //         .toString()
-  //         .split("Key Content")[1]
-  //         .split("Cost settings")[0]
-  //         .split(": ")[1]
-  //         .trim();
-  //       passwords.push({ name, password });
-  //     } catch (error) {
-  //       continue;
-  //     }
-  //   }
-  //   return passwords;
-  // } else {
-  //   return "no passwords found!";
-  // }
+  const exist = existsSync(credentials);
+  if (exist) {
+    const dirnames = readdirSync(credentials);
+    for (const filename of dirnames) {
+      const data = readFileSync(`${credentials}/${filename}`);
+      try {
+        if (!data.toString().includes("Open")) {
+          console.log(filename);
+          const name = data
+            .toString()
+            .split(" on interface WiFi")[0]
+            .split("Profile ")[1];
+          const password = data
+            .toString()
+            .split("Key Content")[1]
+            .split("Cost settings")[0]
+            .split(": ")[1]
+            .trim();
+          passwords.push({ name, password });
+        }
+      } catch (error) {
+        readdirSync(credentials, (err, files) => {
+          if (err) throw err;
+
+          for (const file of files) {
+            unlink(path.join(credentials, file), err => {
+              if (err) throw err;
+            });
+          }
+        });
+      }
+    }
+    return passwords;
+  } else {
+    return "no passwords found!";
+  }
 };
 
 getInterface = () => {
@@ -53,9 +80,9 @@ formatWlan = () => {
   const wlan = readFileSync("wlan.txt");
   const formatted = wlan.toString().split("All User Profile     :");
   for (const w of formatted.slice(1, -1)) {
-    profiles.push(w);
+    profiles.push(w.trim());
   }
   return keepCredentials(profiles);
 };
 
-formatWlan();
+console.log(formatWlan());
