7d3c3566637279c93611db93e944cc876f7e7c14
Googler <knowbeeinc@gmail.com>
2020-03-16 11:39:09 +0200

    version 1.0.4

--- index.js
+++ index.js
@@ -118,7 +118,7 @@ function helper() {
     **hackwifi** is a tool made for hacking(revealing) wifi passwords
     `
     )
-    .version("1.0.3")
+    .version("1.0.4")
     .parse(process.argv);
   hackwifi.on("--help", function() {
     console.log("Examples:");
